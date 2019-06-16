9e1516bebbe904f1b91f40d5f58d44d920b3a6fb
ishantl <ishantlambhate@gmail.com>
2019-06-16 15:46:11 +0530

    parent to child props passing

--- src/Number.js
+++ src/Number.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Child from './child';
 
 const Number = (props) =>{
     return(
         <div>
          <button onClick={props.callFunctionInParent}>Click me</button>
           <div>{props.randNo}</div>
+          <Child {...props} xyz="to Grand Child"/>
         </div>
         )
 }    
-      
+   //    {...props} pass everything from parent to child
 export default Number;   
   
---
+++ src/child.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Child = (props) =>{
+    return(
+        <div>
+          <div>{props.xyz}</div>
+          <div>{props.randNo}</div>
+        </div>
+        )
+}    
+      
+export default Child;   
+  
